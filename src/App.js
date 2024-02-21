62e9348d660c411146b72ecd099564dcb0e0bc7c
mike242424 <mikebyers24@gmail.com>
2024-02-20 20:16:45 -0500

    adde useReducer for questions json data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,48 @@
-import DateCounter from './components/DateCounter';
+import { useEffect, useReducer } from 'react';
+import Header from './components/Header';
+import Main from './components/Main';
+
+const initialState = {
+  questions: [],
+  // 'loading', 'error', 'ready', 'active', 'finished'
+  status: 'loading',
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'dataRecieved':
+      return { ...state, questions: action.payload, status: 'ready' };
+    case 'dataFailed':
+      return { ...state, status: 'error' };
+    default:
+      throw new Error('Action unknown');
+  }
+}
 
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    async function getQuestions() {
+      try {
+        const res = await fetch('http://localhost:8000/questions');
+        const data = await res.json();
+        dispatch({ type: 'dataRecieved', payload: data });
+      } catch (error) {
+        dispatch({ type: 'dataFailed' });
+        console.log(error);
+      }
+    }
+
+    getQuestions();
+  }, []);
   return (
-    <div className="App">
-      <DateCounter />
+    <div className="app">
+      <Header />
+      <Main>
+        <p>1/15</p>
+        <p>Question?</p>
+      </Main>
     </div>
   );
 }
