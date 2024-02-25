e65de4adccc36766c5983d7acdbbb066be49a5d7
mike242424 <mikebyers24@gmail.com>
2024-02-25 17:11:29 -0500

    added finish game page

---
+++ src/components/FinishedScreen.jsx
@@ -0,0 +1,24 @@
+export default function FinishedScreen({ points, totalPointsPossible }) {
+  const scorePercentage = (points / totalPointsPossible) * 100;
+
+  let emoji;
+
+  if (scorePercentage > 90 && scorePercentage <= 100) emoji = '🥇';
+  if (scorePercentage > 80 && scorePercentage <= 90) emoji = '🥈';
+  if (scorePercentage > 70 && scorePercentage <= 80) emoji = '🥉';
+  if (scorePercentage > 60 && scorePercentage <= 70) emoji = '👍🏻';
+  if (scorePercentage >= 0 && scorePercentage <= 60) emoji = '👎🏻';
+
+  // console.log(emoji);
+  // console.log(scorePercentage);
+
+  return (
+    <>
+      <p className="result">
+        You scored <strong>{points}</strong> out of {totalPointsPossible} (
+        {Math.ceil(scorePercentage)}%) {'  '}
+        <span>{emoji}</span>
+      </p>
+    </>
+  );
+}
--- src/components/NextButton.jsx
+++ src/components/NextButton.jsx
@@ -1,11 +1,23 @@
-export default function NextButton({ dispatch, answer }) {
+export default function NextButton({ dispatch, answer, index, numQuestions }) {
   if (answer === null) return null;
-  return (
-    <button
-      onClick={() => dispatch({ type: 'nextQuestion' })}
-      className="btn btn-ui"
-    >
-      Next
-    </button>
-  );
+
+  if (index < numQuestions - 1)
+    return (
+      <button
+        onClick={() => dispatch({ type: 'nextQuestion' })}
+        className="btn btn-ui"
+      >
+        Next
+      </button>
+    );
+
+  if (index === numQuestions - 1)
+    return (
+      <button
+        onClick={() => dispatch({ type: 'finishQuiz' })}
+        className="btn btn-ui"
+      >
+        Finish
+      </button>
+    );
 }
